66f85a62c7565cc7d2d92370e4fde96f61f6fbe4
chhabrasc8 <chhabrasc8@gmail.com>
2026-05-16 10:11:21 +0530

    Update FirstJavaProgram.java

--- Online_Java_Training_20_Apr_2026/src/javaPackage/FirstJavaProgram.java
+++ Online_Java_Training_20_Apr_2026/src/javaPackage/FirstJavaProgram.java
@@ -13,7 +13,7 @@ public class FirstJavaProgram {
 		System.out.println("Mummy");  //Mummy
 		System.out.println("Tummy");  //Tummy
 		System.out.println("12345");  //12345
-
+        System.out.println("Change from Eclipse1");
 	}
 
 }
